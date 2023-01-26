e5d91def94e3f5825d34ffbf63db149021a66ca1
Daniel Moraes <96148567+SirDnx@users.noreply.github.com>
2023-01-26 19:45:05 -0300

    menu engine finished with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@ const btnToggleMenu = document.querySelector('#btnToggleMenu');
 const changeIcon = document.querySelector('#changeIcon');
 const listItems = document.querySelectorAll('.header__list--item');
 
+const buttonsMenu = document.querySelectorAll('[data-link]');
+const containers = document.querySelectorAll('[data-container]');
+
+
 btnToggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle('active');
     changeBtnMenu();
@@ -22,7 +26,20 @@ listItems.forEach( item => {
     item.addEventListener("click", () => {
         listItems.forEach(item => item.classList.remove('active'));
         item.classList.add('active');
-
-        console.log
-    })
+    });
 });
+
+//linking each menu button to their respective container
+
+buttonsMenu.forEach( button => {
+    button.addEventListener("click", (e) => {
+        containers.forEach(container => {
+            container.classList.remove('active');
+        })
+        showContainer(e.currentTarget.getAttribute('data-link'))
+    });
+});
+
+function showContainer(buttonTarget){
+    document.querySelector('[data-container="' + buttonTarget + '-container"]').classList.add('active');
+}
